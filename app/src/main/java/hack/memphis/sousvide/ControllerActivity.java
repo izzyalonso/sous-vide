49b6b136cb2edc62152459f677fab3c4f692a1bc
Ismael Alonso <ismaha91@gmail.com>
2016-09-24 12:12:18 -0500

    Temperature conversion

--- app/src/main/java/hack/memphis/sousvide/ControllerActivity.java
+++ app/src/main/java/hack/memphis/sousvide/ControllerActivity.java
@@ -14,10 +14,15 @@ import hack.memphis.sousvide.databinding.ActivityControllerBinding;
 
 
 /**
- * Created by isma on 9/23/16.
+ * Main controller activity.
+ *
+ * @author Ismael Alonso
+ * @version 1.0.0
  */
 public class ControllerActivity extends AppCompatActivity implements HttpRequest.RequestCallback, View.OnClickListener{
     private ActivityControllerBinding mBinding;
+
+    private char mUnits = 'C';
 
 
     @Override
@@ -39,6 +44,8 @@ public class ControllerActivity extends AppCompatActivity implements HttpRequest
         mBinding.controllerTimePicker.setValue(90);
         mBinding.controllerTimePicker.setWrapSelectorWheel(false);
 
+        mBinding.controllerTemperature.setOnClickListener(this);
+        mBinding.controllerTemperatureUnits.setOnClickListener(this);
         mBinding.controllerSwitch.setOnClickListener(this);
 
         HttpRequest.init(getApplicationContext());
@@ -47,7 +54,30 @@ public class ControllerActivity extends AppCompatActivity implements HttpRequest
 
     @Override
     public void onClick(View view){
+        switch (view.getId()){
+            case R.id.controller_temperature:
+            case R.id.controller_temperature_units:
+                int currentValue = mBinding.controllerTemperaturePicker.getValue();
+                if (mUnits == 'C'){
+                    mBinding.controllerTemperatureUnits.setText("(ºF, switch to C)");
+                    int newValue = Math.min(210, Math.max(85, CtoF(currentValue)));
+                    mBinding.controllerTemperaturePicker.setMinValue(85);
+                    mBinding.controllerTemperaturePicker.setMaxValue(210);
+                    mBinding.controllerTemperaturePicker.setValue(newValue);
+                    mUnits = 'F';
+                }
+                else if (mUnits == 'F'){
+                    mBinding.controllerTemperatureUnits.setText("(ºC, switch to F)");
+                    int newValue = Math.min(100, Math.max(30, FtoC(currentValue)));
+                    mBinding.controllerTemperaturePicker.setMinValue(30);
+                    mBinding.controllerTemperaturePicker.setMaxValue(100);
+                    mBinding.controllerTemperaturePicker.setValue(newValue);
+                    mUnits = 'C';
+                }
+                break;
 
+
+        }
     }
 
     @Override
@@ -58,5 +88,13 @@ public class ControllerActivity extends AppCompatActivity implements HttpRequest
     @Override
     public void onRequestFailed(int requestCode, HttpRequestError error){
 
+    }
+
+    private int CtoF(int value){
+        return (int)(value*1.8)+32;
+    }
+
+    private int FtoC(int value){
+        return (int)((value-32)/1.8);
     }
 }
